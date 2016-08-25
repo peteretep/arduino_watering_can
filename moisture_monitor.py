155e2933e4e506e52642bd066145d5cc187264ba
PI <peteretep@gmail.com>
2016-08-25 09:31:15 +0000

    booma

--- moisture_monitor.py
+++ moisture_monitor.py
@@ -4,9 +4,13 @@ import time
 import csv
 
 ser = serial.Serial('/dev/ttyUSB0')
+old_reading = 0
 while True:
-    moistureReading = ser.readline()
-    row = [time.ctime(), time.time(), int(moistureReading)]
-    with open('moisturelog.csv', 'a') as moisturelog:
-        w = csv.writer(moisturelog)
-        w.writerow(row)
+    moisture_reading = ser.readline()
+    if moisture_reading != old_reading:
+        row = [time.ctime(), time.time(), int(moisture_reading)]
+        with open('moisturelog.csv', 'a') as moisture_log:
+            w = csv.writer(moisture_log)
+            w.writerow(row)
+
+    old_reading = moisture_reading
